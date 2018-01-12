e9d451d5fcf81f3449ba772e10f512d332b908d0
Malgorzata <gosia.haniszewska@gmail.com>
2018-01-12 13:26:00 -0800

    add part to display each doctors phone number

--- build/js/app.js
+++ build/js/app.js
@@ -29,6 +29,7 @@ var _medicalHelper = require('./../js/medical-helper.js');
 
 var apiKey = require('./../.env').apiKey;
 
+// Part about searching by issue
 $(document).ready(function () {
   $("#searching-issue-form").submit(function (event) {
     event.preventDefault();
@@ -58,11 +59,10 @@ $(document).ready(function () {
             } else {
               confirmation += "No";
             }
-            $("#responce-name").prepend("<li>" + doctorPractice.practices[0].name + "<ul>" + "<li>First name: " + doctorPractice.profile.first_name + "</li>" + "<li>Last name: " + doctorPractice.profile.last_name + "</li>" + "<li>Address: " + doctorPractice.practices[0].visit_address.city + ", " + doctorPractice.practices[0].visit_address.zip + ", " + doctorPractice.practices[0].visit_address.street + "</li>" + "<li>Website: " + doctorPractice.practices[0].website + "</li>" + "<li>Accepted new patients: " + confirmation + "</li>" + "</ul>" + "</li>");
+            $("#responce-name").prepend("<li>" + doctorPractice.practices[0].name + "<ul>" + "<li>First name: " + doctorPractice.profile.first_name + "</li>" + "<li>Last name: " + doctorPractice.profile.last_name + "</li>" + "<li>Address: " + doctorPractice.practices[0].visit_address.city + ", " + doctorPractice.practices[0].visit_address.zip + ", " + doctorPractice.practices[0].visit_address.street + "</li>" + "<li>Phone number: " + doctorPractice.practices[0].phones[0].number + "</li>" + "<li>Website: " + doctorPractice.practices[0].website + "</li>" + "<li>Accepted new patients: " + confirmation + "</li>" + "</ul>" + "</li>");
           });
         }
       },
-
       error: function error() {
         $(".issue-results-error").show();
         $("#issue-errors").text("There was an error processing your request about issue. Please try again.");
@@ -70,12 +70,55 @@ $(document).ready(function () {
     });
   });
 });
-// "<ul>"+
-//   "<li>First name: " + doctorPractice.profile.first_name + "</li>" +
-//   "<li>Last name: " + doctorPractice.profile.last_name + "</li>" +
-//   "<li>Address: " + doctorPractice.practices[0].visit_address.city + "</li>" +
-//   "<li>Website: " + doctorPractice.practices[0].website + "</li>" +
-//   "<li>accepting new patients: " + doctorPractice.practices[0].accepts_new_patients + "</li>" +
-// "<ul>"
+
+//Part about searching by doctor's name
+// $(document).ready(function() {
+//   $("#searching-name-form").submit(function(event) {
+//     event.preventDefault();
+//
+//     let name = $("#name").val();
+//     $("#name").val("");
+//     let newDoctor = new DoctorHelper(name);
+//
+//     $.ajax({
+//       //location is fixed to Seattle by providing its latitude and longitude in url
+//       url: `https://api.betterdoctor.com/2016-03-01/doctors?query=${issue}&location=37.773%2C-122.413%2C100&skip=0&limit=10&user_key=${apiKey}`,
+//       type: 'GET',
+//       data: {
+//         format: 'json'
+//       },
+//       success: function(response) {
+//         $(".issue-results-succes").show();
+//         $("#searching-issue").text(issue);
+//         if (response.meta.total === 0) {
+//           $("#responce-name").text("We are so sorry but no doctors meet the criteria. Try again!")
+//         } else {
+//           response.data.forEach(function(doctorPractice) {
+//             let confirmation = "";
+//             let acceptedNewPatient = doctorPractice.practices[0].accepts_new_patients;
+//             if (acceptedNewPatient === true) {
+//               confirmation += "Yes"
+//             } else {
+//               confirmation += "No"
+//             }
+//             $("#responce-name").prepend("<li>"+ doctorPractice.practices[0].name +
+//                                           "<ul>" +
+//                                             "<li>First name: " + doctorPractice.profile.first_name + "</li>" +
+//                                             "<li>Last name: " + doctorPractice.profile.last_name + "</li>" +
+//                                             "<li>Address: " + doctorPractice.practices[0].visit_address.city + ", " + doctorPractice.practices[0].visit_address.zip + ", "  + doctorPractice.practices[0].visit_address.street + "</li>" +
+//                                             "<li>Website: " + doctorPractice.practices[0].website + "</li>" +
+//                                             "<li>Accepted new patients: " + confirmation + "</li>" +
+//                                           "</ul>" +
+//                                         "</li>");
+//           });
+//         }
+//       },
+//       error: function() {
+//         $(".issue-results-error").show();
+//         $("#issue-errors").text("There was an error processing your request about issue. Please try again.");
+//       }
+//     });
+//   });
+// });
 
 },{"./../.env":1,"./../js/medical-helper.js":2}]},{},[3]);
--- tmp/allConcat.js
+++ tmp/allConcat.js
@@ -2,7 +2,7 @@ import { IssueHelper } from './../js/medical-helper.js';
 import { DoctorHelper } from './../js/medical-helper.js';
 var apiKey = require('./../.env').apiKey;
 
-
+// Part about searching by issue
 $(document).ready(function() {
   $("#searching-issue-form").submit(function(event) {
     event.preventDefault();
@@ -37,6 +37,7 @@ $(document).ready(function() {
                                             "<li>First name: " + doctorPractice.profile.first_name + "</li>" +
                                             "<li>Last name: " + doctorPractice.profile.last_name + "</li>" +
                                             "<li>Address: " + doctorPractice.practices[0].visit_address.city + ", " + doctorPractice.practices[0].visit_address.zip + ", "  + doctorPractice.practices[0].visit_address.street + "</li>" +
+                                            "<li>Phone number: " + doctorPractice.practices[0].phones[0].number + "</li>" +
                                             "<li>Website: " + doctorPractice.practices[0].website + "</li>" +
                                             "<li>Accepted new patients: " + confirmation + "</li>" +
                                           "</ul>" +
@@ -44,7 +45,6 @@ $(document).ready(function() {
           });
         }
       },
-
       error: function() {
         $(".issue-results-error").show();
         $("#issue-errors").text("There was an error processing your request about issue. Please try again.");
@@ -52,10 +52,54 @@ $(document).ready(function() {
     });
   });
 });
-// "<ul>"+
-//   "<li>First name: " + doctorPractice.profile.first_name + "</li>" +
-//   "<li>Last name: " + doctorPractice.profile.last_name + "</li>" +
-//   "<li>Address: " + doctorPractice.practices[0].visit_address.city + "</li>" +
-//   "<li>Website: " + doctorPractice.practices[0].website + "</li>" +
-//   "<li>accepting new patients: " + doctorPractice.practices[0].accepts_new_patients + "</li>" +
-// "<ul>"
+
+
+//Part about searching by doctor's name
+// $(document).ready(function() {
+//   $("#searching-name-form").submit(function(event) {
+//     event.preventDefault();
+//
+//     let name = $("#name").val();
+//     $("#name").val("");
+//     let newDoctor = new DoctorHelper(name);
+//
+//     $.ajax({
+//       //location is fixed to Seattle by providing its latitude and longitude in url
+//       url: `https://api.betterdoctor.com/2016-03-01/doctors?query=${issue}&location=37.773%2C-122.413%2C100&skip=0&limit=10&user_key=${apiKey}`,
+//       type: 'GET',
+//       data: {
+//         format: 'json'
+//       },
+//       success: function(response) {
+//         $(".issue-results-succes").show();
+//         $("#searching-issue").text(issue);
+//         if (response.meta.total === 0) {
+//           $("#responce-name").text("We are so sorry but no doctors meet the criteria. Try again!")
+//         } else {
+//           response.data.forEach(function(doctorPractice) {
+//             let confirmation = "";
+//             let acceptedNewPatient = doctorPractice.practices[0].accepts_new_patients;
+//             if (acceptedNewPatient === true) {
+//               confirmation += "Yes"
+//             } else {
+//               confirmation += "No"
+//             }
+//             $("#responce-name").prepend("<li>"+ doctorPractice.practices[0].name +
+//                                           "<ul>" +
+//                                             "<li>First name: " + doctorPractice.profile.first_name + "</li>" +
+//                                             "<li>Last name: " + doctorPractice.profile.last_name + "</li>" +
+//                                             "<li>Address: " + doctorPractice.practices[0].visit_address.city + ", " + doctorPractice.practices[0].visit_address.zip + ", "  + doctorPractice.practices[0].visit_address.street + "</li>" +
+//                                             "<li>Website: " + doctorPractice.practices[0].website + "</li>" +
+//                                             "<li>Accepted new patients: " + confirmation + "</li>" +
+//                                           "</ul>" +
+//                                         "</li>");
+//           });
+//         }
+//       },
+//       error: function() {
+//         $(".issue-results-error").show();
+//         $("#issue-errors").text("There was an error processing your request about issue. Please try again.");
+//       }
+//     });
+//   });
+// });
